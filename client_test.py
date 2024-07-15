f5de133e6ae941b6c93b7f0d8c1a524fbdd85c03
psyos9 <psyos9@exmail.nottingham.ac.uk>
2024-07-15 16:16:17 +0100

    completed task 1

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint,getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    datapoint = getDataPoint(quotes[0])
+    self.assertEqual(("ABC",120.48,121.2,120.84),datapoint)
+
+    datapoint2 = getDataPoint(quotes[1])
+    self.assertEqual(("DEF",117.87,121.68,119.775),datapoint2)
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,7 +21,14 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    datapoint1 = getDataPoint(quotes[0])
+    datapoint2 = getDataPoint(quotes[1])
+    
 
+    self.assertEqual(getRatio(0,0),None)
+    self.assertEqual(getRatio(datapoint1[1],datapoint1[2]),datapoint1[1]/datapoint1[2])
+    self.assertEqual(getRatio(datapoint2[1],datapoint2[2]),datapoint2[1]/datapoint2[2])
+    
 
   """ ------------ Add more unit tests ------------ """
 
